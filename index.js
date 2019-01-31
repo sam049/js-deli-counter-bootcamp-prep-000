aae0568f44fa372188028e501f141779737cdb91
Sam Miller <sammillerks@gmail.com>
2019-01-31 22:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     var NumberPlusCustomer = []
-    for (let i = 0, i < line.length, i++) {
+    for (let i = 0; i < line.length; i++) {
       NumberPlusCustomer.push(i+1 + "." + line[i]);
     }
   }
