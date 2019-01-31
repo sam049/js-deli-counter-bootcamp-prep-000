b3c4fa2bdebbd8dbc1e585b8e981ed800b1ea12b
Sam Miller <sammillerks@gmail.com>
2019-01-31 22:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return "Currently serving " + katzDeliLine[0] + "."
-    katzDeliLine.unshift();
+    katzDeliLine.shift();
+    return katzDeliLine
   } else {
     return "There is nobody waiting to be served!";
   }
